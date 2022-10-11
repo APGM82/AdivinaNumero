61654df9734411d556dd0557f231e9582c9fe686
APGM82 <90765662+APGM82@users.noreply.github.com>
2022-10-11 12:45:57 +0200

    v0.1.0

--- app/src/main/java/com/example/adivinanumero/MainActivity.kt
+++ app/src/main/java/com/example/adivinanumero/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.adivinanumero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
+lateinit var bt : Button
+lateinit var bt2 : Button
+lateinit var mom: TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bt = findViewById(R.id.btnBoton)
+        bt2=findViewById((R.id.btnComprobar))
+        mom=findViewById(R.id.txtMasOMenos)
+        bt.setOnClickListener {
+
+        }
+        bt2.setOnClickListener {
+
+        }
+
     }
 }
