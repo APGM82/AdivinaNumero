f3a3d7f45905bda404da2cf6abb1c1a5f90ed451
APGM82 <90765662+APGM82@users.noreply.github.com>
2022-10-13 10:18:23 +0200

    sigue fallando

--- app/src/main/java/com/example/adivinanumero/MainActivity.kt
+++ app/src/main/java/com/example/adivinanumero/MainActivity.kt
@@ -9,7 +9,7 @@ import kotlin.random.Random
 var secreto10:Int=Random.nextInt(1-11)
 var secreto50:Int=Random.nextInt(1-51)
 var secreto100:Int=Random.nextInt(1-101)
-var dificultad:Int=1;
+var dificultad:Int=1
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,28 +17,35 @@ class MainActivity : AppCompatActivity() {
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*binding.seekBar.setOnSeekBarChangeListener(object :
+        binding.seekBar.progress=0
+        binding.textView3.text="Fácil"
+        binding.seekBar.setOnSeekBarChangeListener(object :
             SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seek: SeekBar,
-                                           progress: Int, fromUser: Boolean) {
-                binding.caja1.text = "Cambiando progreso"
+            override fun onProgressChanged(
+                seek: SeekBar,
+                progress: Int, fromUser: Boolean
+            ) {
+                //binding.caja1.text = "Cambiando progreso"
             }
-
             override fun onStartTrackingTouch(seek: SeekBar) {
-                binding.caja2.setText("Starting tracking")
+                //binding.caja2.setText("Starting tracking")
             }
-
             override fun onStopTrackingTouch(seek: SeekBar) {
-                binding.caja2.setText("Stopping tracking")
-                binding.caja1.setText(binding.seekBarDiscreta.progress.toString())
+                //binding.caja2.setText("Stopping tracking")
+                //binding.caja1.setText(binding.seekBarDiscreta.progress.toString())
+                when(((binding.seekBar.progress)+1)){
+                    1 ->
+                        binding.textView3.text="Fácil"
+                    2 ->
+                        binding.textView3.text="Media"
+                    3 ->
+                        binding.textView3.text="Dificil"
+                }
+                //dificultad=((binding.seekBar.progress)+1) //Esto al ponerlo me da error
             }
-        */
+        })
 
-
-
-        binding.textView3.text=((binding.seekBar.progress)+1).toString()
-
-        binding.btnBoton.setOnClickListener {
+        binding.btnReiniciar.setOnClickListener {
             secreto10=Random.nextInt(1-11)
             secreto50=Random.nextInt(1-51)
             secreto100=Random.nextInt(1-101)
@@ -46,32 +53,41 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnComprobar.setOnClickListener {
-
-
-            when(dificultad){
-
-                1->
-                    if ((Integer.parseInt(binding.TextNumero.text.toString())<secreto10)){
-                        binding.txtMasOMenos.text="Mayor"
-                    }
-                    else if ((Integer.parseInt(binding.TextNumero.text.toString())>secreto10)){
-                        binding.txtMasOMenos.text="Menor"
-                    }else{binding.txtMasOMenos.text="Exacto" }
-                2->
-                    if ((Integer.parseInt(binding.TextNumero.text.toString())<secreto50)){
-                        binding.txtMasOMenos.text="Mayor"
-                    }
-                    else if ((Integer.parseInt(binding.TextNumero.text.toString())>secreto50)){
-                        binding.txtMasOMenos.text="Menor"
-                    }else{ binding.txtMasOMenos.text="Exacto"}
-
-                3->
-                    if ((Integer.parseInt(binding.TextNumero.text.toString())<secreto100)){
-                        binding.txtMasOMenos.text="Mayor"
-                    }
-                    else if ((Integer.parseInt(binding.TextNumero.text.toString())>secreto100)){
-                        binding.txtMasOMenos.text="Menor"
-                    }else{ binding.txtMasOMenos.text="Exacto"}
+            if (dificultad == 1) {
+                if ((Integer.parseInt(binding.TextNumero.text.toString())<secreto10)){
+                    binding.txtMasOMenos.text="Mayor"
+                    binding.imageView.setImageResource(R.drawable.up)
+                } else if ((Integer.parseInt(binding.TextNumero.text.toString())>secreto10)){
+                    binding.txtMasOMenos.text="Menor"
+                    binding.imageView.setImageResource(R.drawable.down)
+                }else{
+                    binding.txtMasOMenos.text="Exacto"
+                    binding.imageView.setImageResource(R.drawable.win)
+                }
+            }
+            else if (dificultad == 2) {
+                if ((Integer.parseInt(binding.TextNumero.text.toString())<secreto50)){
+                    binding.txtMasOMenos.text="Mayor"
+                    binding.imageView.setImageResource(R.drawable.up)
+                } else if ((Integer.parseInt(binding.TextNumero.text.toString())>secreto50)){
+                    binding.txtMasOMenos.text="Menor"
+                    binding.imageView.setImageResource(R.drawable.down)
+                }else{
+                    binding.txtMasOMenos.text="Exacto"
+                    binding.imageView.setImageResource(R.drawable.win)
+                }
+            }
+            else if (dificultad == 3) {
+                if ((Integer.parseInt(binding.TextNumero.text.toString())<secreto100)){
+                    binding.txtMasOMenos.text="Mayor"
+                    binding.imageView.setImageResource(R.drawable.up)
+                } else if ((Integer.parseInt(binding.TextNumero.text.toString())>secreto100)){
+                    binding.txtMasOMenos.text="Menor"
+                    binding.imageView.setImageResource(R.drawable.down)
+                }else{
+                    binding.txtMasOMenos.text="Exacto"
+                    binding.imageView.setImageResource(R.drawable.win)
+                }
             }
         }
     }
