56a2389293d6a2d53a1348d43860589c7c4a748c
APGM82 <90765662+APGM82@users.noreply.github.com>
2022-10-13 09:41:42 +0200

    Medio rota aun

--- app/src/main/java/com/example/adivinanumero/MainActivity.kt
+++ app/src/main/java/com/example/adivinanumero/MainActivity.kt
@@ -2,27 +2,78 @@ package com.example.adivinanumero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
+import com.example.adivinanumero.databinding.ActivityMainBinding
 import kotlin.random.Random
 
-lateinit var bt : Button
-lateinit var bt2 : Button
-lateinit var mom: TextView
+var secreto10:Int=Random.nextInt(1-11)
+var secreto50:Int=Random.nextInt(1-51)
+var secreto100:Int=Random.nextInt(1-101)
+var dificultad:Int=1;
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        bt = findViewById(R.id.btnBoton)
-        bt2=findViewById((R.id.btnComprobar))
-        mom=findViewById(R.id.txtMasOMenos)
-        bt.setOnClickListener {
+        /*binding.seekBar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                binding.caja1.text = "Cambiando progreso"
+            }
 
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                binding.caja2.setText("Starting tracking")
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                binding.caja2.setText("Stopping tracking")
+                binding.caja1.setText(binding.seekBarDiscreta.progress.toString())
+            }
+        */
+
+
+
+        binding.btnBoton.setOnClickListener {
+            secreto10=Random.nextInt(1-11)
+            secreto50=Random.nextInt(1-51)
+            secreto100=Random.nextInt(1-101)
+            binding.txtMasOMenos.text=" "
         }
-        bt2.setOnClickListener {
-
+        binding.seekBar.setOnClickListener(){
+            dificultad= binding.seekBar.progress
         }
+        binding.btnComprobar.setOnClickListener {
 
+
+            when(dificultad){
+
+                1->
+                    if ((Integer.parseInt(binding.TextNumero.text.toString())<secreto10)){
+                        binding.txtMasOMenos.text="Mayor"
+                    }
+                    else if ((Integer.parseInt(binding.TextNumero.text.toString())>secreto10)){
+                        binding.txtMasOMenos.text="Menor"
+                    }else{binding.txtMasOMenos.text="Exacto" }
+                2->
+                    if ((Integer.parseInt(binding.TextNumero.text.toString())<secreto50)){
+                        binding.txtMasOMenos.text="Mayor"
+                    }
+                    else if ((Integer.parseInt(binding.TextNumero.text.toString())>secreto50)){
+                        binding.txtMasOMenos.text="Menor"
+                    }else{ binding.txtMasOMenos.text="Exacto"}
+
+                3->
+                    if ((Integer.parseInt(binding.TextNumero.text.toString())<secreto100)){
+                        binding.txtMasOMenos.text="Mayor"
+                    }
+                    else if ((Integer.parseInt(binding.TextNumero.text.toString())>secreto100)){
+                        binding.txtMasOMenos.text="Menor"
+                    }else{ binding.txtMasOMenos.text="Exacto"}
+            }
+        }
     }
+
 }
